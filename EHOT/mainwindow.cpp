960b05fe2cc09fb73b82f1c1b8a4bde61def2d53
pAndrey200 <potseluev-2002@mail.ru>
2021-10-05 19:50:11 +0300

    add posgresql
    почти готовая 2 лаба

--- EHOT/mainwindow.cpp
+++ EHOT/mainwindow.cpp
@@ -1,15 +1,48 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+#include <QVariant>
+#include <QDebug>
+#include <QTextStream>
+#include <QSqlQueryModel>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    db = QSqlDatabase::addDatabase("QPSQL");
+    db.setDatabaseName("tdb");
+    db.setHostName("localhost");
+    db.setUserName("postgres");
+    db.setPassword("iopkl;");
+    if(!db.open())
+         ui->label->setText( db.lastError().text() + "\n");
+    else
+        ui->label->setText("db open");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QSqlQuery msg(db);
+    QString cmd = ui->textEdit->toPlainText();
+    if(!msg.exec(cmd)){
+                ui->label->setText( msg.lastError().text() + "\n");
+            }
+            else if(msg.isSelect()){
+                QSqlQueryModel *model = new QSqlQueryModel;
+                model->setQuery(msg);
+                ui->tableView->setModel(model);
+            }
+            else{
+                ui->label->setText("Done!\n");
+            }
 }
 
--- EHOT/mainwindow.h
+++ EHOT/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlRecord>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +19,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QSqlDatabase db;
 };
 #endif // MAINWINDOW_H
