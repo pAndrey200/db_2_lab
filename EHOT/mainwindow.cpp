5146fe9ab43080e83603d3ffcc42679386aa0307
pAndrey200 <potseluev-2002@mail.ru>
2021-10-05 22:14:06 +0300

    update posgress data

--- EHOT/mainwindow.cpp
+++ EHOT/mainwindow.cpp
@@ -1,10 +1,5 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
-
-#include <QVariant>
-#include <QDebug>
-#include <QTextStream>
 #include <QSqlQueryModel>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -12,11 +7,12 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     db = QSqlDatabase::addDatabase("QPSQL");
-    db.setDatabaseName("tdb");
-    db.setHostName("localhost");
-    db.setUserName("postgres");
-    db.setPassword("iopkl;");
+    db.setDatabaseName("fn1133_2017");
+    db.setHostName("195.19.32.74");
+    db.setUserName("student");
+    db.setPassword("bmstu");
     if(!db.open())
          ui->label->setText( db.lastError().text() + "\n");
     else
@@ -34,15 +30,15 @@ void MainWindow::on_pushButton_clicked()
     QSqlQuery msg(db);
     QString cmd = ui->textEdit->toPlainText();
     if(!msg.exec(cmd)){
-                ui->label->setText( msg.lastError().text() + "\n");
-            }
-            else if(msg.isSelect()){
-                QSqlQueryModel *model = new QSqlQueryModel;
-                model->setQuery(msg);
-                ui->tableView->setModel(model);
-            }
-            else{
-                ui->label->setText("Done!\n");
-            }
+        ui->label->setText( msg.lastError().text() + "\n");
+    }
+    else if(msg.isSelect()){
+        QSqlQueryModel *model = new QSqlQueryModel;
+        model->setQuery(msg);
+        ui->tableView->setModel(model);
+    }
+    else{
+        ui->label->setText("Done!\n");
+    }
 }
 
